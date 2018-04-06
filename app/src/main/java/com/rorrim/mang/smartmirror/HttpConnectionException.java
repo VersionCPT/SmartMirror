b97122c432d557e597c180acbf71bf4b88525e3a
201411317 <whalsrb1226@naver.com>
2018-04-06 16:11:38 +0900

    Http Request

--- app/src/main/java/com/rorrim/mang/smartmirror/HttpConnectionException.java
+++ app/src/main/java/com/rorrim/mang/smartmirror/HttpConnectionException.java
@@ -1,6 +1,7 @@
 package com.rorrim.mang.smartmirror;
 
 public class HttpConnectionException extends Exception {
+
     @Override
     public String getMessage(){
         return "Connection Error";
